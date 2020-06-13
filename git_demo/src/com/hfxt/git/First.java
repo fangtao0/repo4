b0f41a32271d89eb8cbd0494d28e543a3a804e90
段方涛 <995632232@qq.com>
2020-06-14 01:57:35 +0800

    本地工程

---
+++ git_demo/src/com/hfxt/git/First.java
@@ -0,0 +1,10 @@
+package com.hfxt.git;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+    }
+}
