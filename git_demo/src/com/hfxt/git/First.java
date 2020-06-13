e086b964e640c24c76ff84d45d50e082f27addc9
段方涛 <995632232@qq.com>
2020-06-14 02:21:02 +0800

    修改方法

--- git_demo/src/com/hfxt/git/First.java
+++ git_demo/src/com/hfxt/git/First.java
@@ -6,6 +6,7 @@ public class First {
         System.out.println("Hello Git");
         System.out.println("Hello Git");
         System.out.println("Hello Git");
+        //hellos
     }
 
     public void text(){
